9d585806f7f72fe9aebe18bbd44134f14b1f99f1
Aden Cronje <adencronje05@gmail.com>
2024-07-22 14:26:00 +0200

    Added function to create store

--- store.js
+++ store.js
@@ -8,8 +8,8 @@ const SUBTRACT = "DECREASE";
 const RESET = "RESET";
 
 // Handle actions and update state
-function reducer(state = initialState, SUBTRACT) {
-  switch (SUBTRACT.type) {
+function reducer(state = initialState, action) {
+  switch (action.type) {
     case ADD:
       return { ...(state += 1) };
     case SUBTRACT:
@@ -19,4 +19,22 @@ function reducer(state = initialState, SUBTRACT) {
     default:
       return state;
   }
+}
+
+// Creates the store
+function initializeStore(reducer) {
+  let currentState = initialState;
+  let listeners = [];
+  return {
+    getState: () => currentState,
+    dispatch: (action) => {
+      currentState = reducer(currentState, action).foreach(() => {});
+      subscribe: (listener) => {
+        listener.push(listeners);
+        return () => {
+          listener.filter(listeners);
+        };
+      };
+    },
+  };
 }
